3111407f23010087d2c7a447f8b1993b76803f11
dollypizzle <dolapo.mabawonku@gmail.com>
2022-04-20 11:25:20 +0100

    Simple Array Sum  Challenge Solved

--- simple-array-sum/simple-array-sum.js
+++ simple-array-sum/simple-array-sum.js
@@ -1,17 +1,48 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+'use strict';
 
-int main() {
-	int n,i,a,sum=0;		
-			scanf("%d",&n); 
-			if(n<=1000){
-			for(i=0;i<n;++i){
-                scanf("%d",&a);
-                sum=sum+a;
-            }
-     printf("%d",sum);
-    return 0;
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+    
+    return ar.reduce(function (acc, val) {
+        return acc + val;
+    });
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = simpleArraySum(ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
 }
